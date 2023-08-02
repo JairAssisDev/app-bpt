eb4af1998781a98149c069edf2d680c20d6f3feb
JairAssisDev <jairv141@gmail.com>
2023-08-02 17:27:51 -0300

    atlz

--- renderer.js
+++ renderer.js
@@ -5,3 +5,12 @@
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
  */
+const form = document.querySelector('form');
+        const submitBtn = document.getElementById('submit-btn');
+
+        form.addEventListener('input', () => {
+            const inputs = form.querySelectorAll('input');
+            const allFieldsFilled = Array.from(inputs).every(input => input.value.trim() !== '');
+
+            submitBtn.disabled = !allFieldsFilled;
+        });
