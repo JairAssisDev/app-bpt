2086205cbd59fe754febd5deb96d79a6bc0e81aa
jairassidev <jairv141@gmail.com>
2023-09-24 22:36:42 -0300

    teste

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+// Adicione esta parte para reiniciar a janela principal
+ipcMain.on('restart-app', () => {
+  mainWindow.close(); // Feche a janela atual
+  createWindow(); // Crie uma nova janela principal
+});
